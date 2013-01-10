2a4d427f9fe86011656fa2d171d012ac241669ac
Fernando Briano <fernando@picandocodigo.net>
2013-01-10 03:46:43 -0200

    Redirect to github pages

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ Cuba.plugin Cuba::Render
 
 Cuba.define do
   on get do
-    on "es" do
-      res.write render("views/home-es.erb")
-    end
-
-    res.write render("views/home.erb")
+    res.redirect "http://rubyconfuruguay.github.com/"
   end
 end
